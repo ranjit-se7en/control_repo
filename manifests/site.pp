84b2627729789ad999df2e49e92774eac5e1d3bf
Ranjit <40806366+ranjit-se7en@users.noreply.github.com>
2020-11-20 12:37:48 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,18 +3,22 @@ node default {
 }
 
 node 'master.puppet.vm' {
-  include role::master_server
+  #include role::master_server
+  
+}
+
+node /^web/ {
+  include role::app_server
   file { '/root/README':
     ensure => present,
     content => $os.family,
   }
 }
 
-node /^web/ {
-  include role::app_server
-}
-
 node /^db/ {
     include role::db_server
-
+    file { '/root/README':
+    ensure => present,
+    content => $os.family,
+  }
 }
