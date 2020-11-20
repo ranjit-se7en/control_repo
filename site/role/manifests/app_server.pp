ec20d8a2f9f04aa3c903a3452290888364f11292
Ranjit <40806366+ranjit-se7en@users.noreply.github.com>
2020-11-20 12:39:17 +0530

    Update app_server.pp

--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -2,4 +2,8 @@ class role::app_server {
   include profile::web
   include profile::app
   include profile::base
+  file { '/root/README':
+    ensure => present,
+    content => $os.family,
+  }
 }
